17168863cdf24d60649dc505de7acac3dc37e4f8
Hasmitha Krishna <hasmithakrishna@gmail.com>
2026-05-08 15:22:55 -0400

    Backend updated with url shortening, redirect routes and model

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import urlRoutes from './routes/url.js';
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req,res) =>{
-    res.send("Hello World")
-})
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+}));
+app.use(express.json());
 
-app.listen(5000, ()=>{
-    console.timeLog("Server is running on port 5000");
+app.use("/", urlRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
 });
